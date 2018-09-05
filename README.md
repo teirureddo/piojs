fc914aab74e49d74eb568043ec165df9cdc56b6e
teirureddo <teirureddo@protonmail.com>
2018-09-05 00:07:58 -0400

    可以移动的Pio

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # Pio.js
 
 这是一个自动加载可爱的Pio以及其他Live2D模型的JavaScript脚本
+
+目前仍在开发中...
+
+所需功能
+- Pio刷新前记住位置
+- Pio.js的专用配置文件
+- 自动切换模型
+- 允许修改硬编码的一些参数
+- 预制多个模型
+- 专用配置文件的文档
+
+
+这个项目使用以及借鉴很多项目
+- EYHN/hexo-helper-live2d
+- xb2016/poster-girl-l2d-2233
+- fghrsh/live2d_api
+
+由于原项目采用GPLv2，因此本项目也采用GPLv2开源
+
+模型转载于fghrsh/live2d_api，为制作者所有，不得用于商业用途
+如您認為模型内容侵犯您的版权请联络Twitter的 @teirureddo 删除
+
+----
+GPLv2 © Teirureddo
--- pio.js
+++ pio.js
@@ -1,4 +1,27 @@
-$("body").append("<canvas style='position:fixed;bottom:0;left:0;z-index:999;' id='piojs' width='280' height='250' class='piojs'></canvas>");
+$("body").append("<canvas style='position:fixed;bottom:0;left:0;z-index:999;' id='piojs' width='280' height='250'></canvas>");
 $("body").append("<script src='./piojs/live2d.js'>")
 
 loadlive2d("piojs", "./piojs/models/pio/default.json");
+
+//使canvas可以移动
+$(document).ready(function(){
+    var pioCanvas=$('#piojs')[0];
+    pioCanvas.onmousedown=function(e){
+        var offsetX=e.offsetX;   
+        var windowWidth=$(window).width();
+        document.onmousemove=function(e){
+            var clientX=e.clientX;
+            pioCanvas.style.left=clientX-offsetX+"px";
+            if(pioCanvas.offsetLeft<0){
+                pioCanvas.style.left=0;
+            }
+            else if(pioCanvas.offsetLeft+pioCanvas.offsetWidth>windowWidth){
+                pioCanvas.style.left=windowWidth-pioCanvas.offsetWidth+"px";
+            }
+        };
+        document.onmouseup=function(e){
+            document.onmousemove = null;
+            document.onmouseup = null;
+        }
+    }
+});
