8e81fa25c94e204ae6a29f998fe6fd11706ba9bd
teirureddo <teirureddo@protonmail.com>
2018-09-10 15:28:24 -0400

    修正代码风格

--- pio.js
+++ pio.js
@@ -1,11 +1,11 @@
 $("body").append("<canvas style='position:fixed;bottom:0;left:0;z-index:999;' id='piojs' width='280' height='250'></canvas>");
-$("body").append("<script src='./piojs/live2d.js'>")
+$("body").append("<script src='./piojs/live2d.js'>");
 
 loadlive2d("piojs", "./piojs/models/pio/default.json");
 
 //使canvas可以移动
 $(document).ready(function(){
-    var pioCanvas=$('#piojs')[0];
+    var pioCanvas=$("#piojs")[0];
     pioCanvas.onmousedown=function(e){
         var offsetX=e.offsetX; //鼠标于元素内X轴
         var windowWidth=$(window).width(); //屏幕宽度
@@ -26,6 +26,6 @@ $(document).ready(function(){
         document.onmouseup=function(e){
             document.onmousemove = null;
             document.onmouseup = null;
-        }
+        };
     }
 });
