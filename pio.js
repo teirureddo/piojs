51e38c5b84e3a4d2ceb9b43dae4334d5fd8650a8
teirureddo <teirureddo@protonmail.com>
2018-09-28 14:35:37 -0400

    允许修改硬编码的一些参数

--- pio.js
+++ pio.js
@@ -1,7 +1,11 @@
 $("body").append("<canvas style='position:fixed;bottom:0;left:0;z-index:999;' id='piojs' width='280' height='250'></canvas>");
 $("body").append("<script src='./piojs/live2d.js'>");
 
-loadlive2d("piojs", "./piojs/models/pio/default.json");
+$.getJSON("./piojs/conf.json",function(data){
+    $("#piojs").width = data.width;
+    $("#piojs").height = data.height;
+    loadlive2d("piojs", data.model);
+});
 
 //使canvas可以移动
 $(document).ready(function(){
