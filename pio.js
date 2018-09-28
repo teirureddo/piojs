912cff13db7f2594240313abeec6530afc840212
teirureddo <teirureddo@protonmail.com>
2018-09-28 13:39:38 -0400

    配置文件及多模型自动切换

--- pio.js
+++ pio.js
@@ -1,18 +1,28 @@
-$("body").append("<canvas style='position:fixed;bottom:0;left:0;z-index:999;' id='piojs' width='280' height='250'></canvas>");
 $("body").append("<script src='./piojs/live2d.js'>");
+$("body").append("<canvas style='position:fixed;bottom:0;left:0;z-index:999;' id='piojs' width='280' height='250'></canvas>");
 
-loadlive2d("piojs", "./piojs/models/pio/default.json");
-
-//使canvas可以移动
 $(document).ready(function(){
     var pioCanvas=$("#piojs")[0];
+
+    //加載模型
+    $.getJSON("./piojs/info.json",function(data){
+      var dataRandID = (Math.ceil(Math.random() * (data.length)) - 1)
+
+      $("#piojs").width = data[dataRandID].width;
+      $("#piojs").height = data[dataRandID].height;
+
+      loadlive2d("piojs", data[dataRandID].model);
+    });
+ 
 
     //初始化Pio位置记录
     if (!localStorage.getItem("pioLeft")){
         localStorage.setItem("pioLeft", "0px");
     }
-    pioCanvas.style.left=localStorage.getItem('pioLeft');
+    pioCanvas.style.left=localStorage.getItem("pioLeft");
 
+
+    //使canvas可以移动
     pioCanvas.onmousedown=function(e){
         var offsetX=e.offsetX; //鼠标于元素内X轴
         var windowWidth=$(window).width(); //屏幕宽度
